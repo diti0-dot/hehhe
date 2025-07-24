e58e43009df3ba707f4a59b33e603166ec3182a0
dn24002 <dn24002@edu.lu.lv>
2025-07-24 15:56:41 +0000

    privacy policy -_-

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     omniauth_callbacks: "users/omniauth_callbacks"
   }
+  get '/privacy', to: 'pages#privacy'
   resources :events
   resources :preferred_emails
   post '/webhooks/gmail_notification', to: 'webhooks#gmail_notification'
